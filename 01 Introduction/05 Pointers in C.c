0939e8a7e7f16cc14e4f35058d24c4a66abb20ec
TONMOY M <dev.tonmoy@proton.me>
2023-08-03 14:35:04 +0530

    05

---
+++ 01 Introduction/05 Pointers in C.c
@@ -0,0 +1,23 @@
+/* *
+ * Author: Tonmoy M
+ * URL: https://qinetique.github.io
+ * */
+
+#include <stdio.h>
+
+void update(int *a, int *b){
+    int temp = *a;
+    *a = *a + *b;
+    *b = temp > *b ? temp - *b : *b - temp;
+}
+
+int main(){
+    int a, b;
+    int *pa = &a, *pb = &b;
+
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
